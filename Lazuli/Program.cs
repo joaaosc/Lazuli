a7b062c832fe71f74aebf8c0da9636c7d579190c
João Pedro Costa <joaopedrocosta@outlook.com.br>
2023-08-03 17:17:09 -0300

    Testando para ver se o Git está configurado.

--- Lazuli/Program.cs
+++ Lazuli/Program.cs
@@ -7,7 +7,7 @@ class Program
 {
     static void Main()
     {
-
+        // apenas testando pra ver se o git funciona
         Application.Init();
         Application.Run<FirstTimeWindow>();
         Application.Shutdown();
